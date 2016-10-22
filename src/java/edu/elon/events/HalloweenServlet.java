754f1e44ac03b07ef624b201473f3f8ff052a081
Nathan Young <nyoung5@carpenter-10.elon.edu>
2016-10-21 21:54:20 -0400

    completed servlet for spooky website. Not sure about welcome-file goelon.

---
+++ src/java/edu/elon/events/HalloweenServlet.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.elon.events;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author nyoung5
+ */
+public class HalloweenServlet extends HttpServlet {
+
+    
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String url = "/index.html";
+        
+        // get current action
+        String action = request.getParameter("action");
+        
+        if (action == null) {
+            action = "join";  // default action
+        }
+
+        // perform action and set URL to appropriate page
+        if (action.equals("join")) {
+            url = "/index.html";    // the "join" page
+        } 
+        else if (action.equals("add")) {
+            // get parameters from the request
+            url = "/subscribe.html";
+            String firstName = request.getParameter("first-name");
+            String lastName = request.getParameter("last-name");
+            String email = request.getParameter("email");
+            System.out.println("The first-name value is: " + firstName);
+            System.out.println("The last-name value is:" + lastName);
+            System.out.println("The email value is: " + email);
+            System.out.println("The action value is: " + action);
+        }
+        getServletContext()
+                .getRequestDispatcher(url)
+                .forward(request, response);
+        
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
